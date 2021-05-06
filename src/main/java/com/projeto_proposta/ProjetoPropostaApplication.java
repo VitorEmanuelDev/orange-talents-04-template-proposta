0caf15daaae37724e5508c302c15f0971df9e1f7
root <root@ZUP-6253.localdomain>
2021-05-06 19:24:21 -0300

    Set up inicial do projeto

---
+++ src/main/java/com/projeto_proposta/ProjetoPropostaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_proposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPropostaApplication.class, args);
+	}
+
+}
